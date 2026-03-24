0ac21ae634f3f37e3efd5895f35d6d8e837fc0f6
Kai Garrod <kaigarrod@gmail.com>
2026-03-24 16:12:57 +0000

    Refactor code to make more readable

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get('/api/movies', (req, res) => {
-    res.json(['Batman', 'Inception']);
+const client = new MongoClient(`mongodb://${process.env.DB_HOST}:27017`);
+
+app.get('/api/films', async (req, res) => {
+    try {
+        await client.connect();
+
+        const db = client.db('moviesdb');
+        const collection = db.collection('films');
+
+        const films = await collection.find().toArray();
+
+        res.json(films);
+    } catch (err) {
+        res.status(500).send('Error fetching data');
+    }
 });
 
 app.listen(port, () => {
